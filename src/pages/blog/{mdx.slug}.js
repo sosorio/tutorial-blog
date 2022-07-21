72766128f4679b07eabf1590ff92ed5ecb05a3fa
Santiago Osorio <sosorio1973@gmail.com>
2022-07-20 19:57:21 -0500

    Post con imagenes

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,12 +2,17 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const BlogPostPage = ({data}) => {
-  console.log(data);
+  const image = getImage(data.mdx.frontmatter.hero_image)
+  //console.log(data);
     return (
       <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>{data.mdx.frontmatter.date}</p><GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}
+    />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </Layout>
     )
@@ -17,8 +22,16 @@ const BlogPostPage = ({data}) => {
   query ($id: String) {
     mdx(id: {eq: $id}) {
       frontmatter {
-        date
         title
+        date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
