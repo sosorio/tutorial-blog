02ca9adc556917e7f933e871ff1e863ad8cf6155
Santiago Osorio <sosorio1973@gmail.com>
2022-07-24 17:54:10 -0500

    otro ajuste mas del menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,21 +21,18 @@ const Navbar = () => {
                 <li class="p-3">Contactenos</li>
                 </ul>
         
-        <div onClick={handleNav} class="block md:hidden p-4 w-[90%]">
+        <div onClick={handleNav} class="block md:hidden p-4">
             {!nav ? 'X' : <StaticImage src="../images/bars-solid.svg"
      alt="Menu" height={50} />}
-          <div class={!nav ? "fixed left-0 top-0 w-[50%] h-full border-r border-r-gray-900 bg-gray-100 m-4 ease-in-out duration-500" 
+          <div class={!nav ? "fixed left-0 top-15 w-[100%] h-full border-r border-r-gray-900 bg-gray-100 m-4 ease-in-out duration-500" 
      : "fixed left-[-100%]"}>
-     <h1 class="w-full text-3xl font-bold p-4"><StaticImage src="../images/logo-floresHoy.png"
-     alt="logo" height={40}  /> FloresHoy</h1>
-     
         <ul class="pt-4">
-        <li class="p-3 border-b">Cumpleaños</li>
-                <li class="p-3 border-b">Aniversario</li>
-                <li class="p-3 border-b">Amor</li>
-                <li class="p-3 border-b">Agradecimiento</li>
-                <li class="p-3 border-b">Condolencia</li>
-                <li class="p-3 border-b">Contactenos</li>  
+        <li class="pl-10 p-3 border-b">Cumpleaños</li>
+                <li class="pl-10 p-3 border-b">Aniversario</li>
+                <li class="pl-10 p-3 border-b">Amor</li>
+                <li class="pl-10 p-3 border-b">Agradecimiento</li>
+                <li class="pl-10 p-3 border-b">Condolencia</li>
+                <li class="pl-10 p-3 border-b">Contactenos</li>  
         </ul>
      </div>
         </div>
